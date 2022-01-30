bde323c9e458dcd72dd629fcfe0d4f2216ba7c99
Tuan Nguyen <ntuan.2502@gmail.com>
2022-01-30 14:28:02 +0700

    add missing i18n.config

---
+++ i18n.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  i18n: {
+    locales: [
+      "vi",
+      "en",
+      "chs",
+      "cht",
+      "fr",
+      "de",
+      "id",
+      "ja",
+      "ko",
+      "pt",
+      "ru",
+      "es",
+      "th",
+    ],
+    defaultLocale: "vi",
+  },
+};
