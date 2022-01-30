0d46065f7370a5f24b82640c169e58dffe561a06
Tuan Nguyen <ntuan.2502@gmail.com>
2022-01-30 19:25:28 +0700

    add adsense

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,11 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Paimon Data</title>
         <link rel="shortcut icon" href="/images/ui/paimon.png" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3247125224510311"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
