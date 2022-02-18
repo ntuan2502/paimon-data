8fd8baf96045a506d77a95ac0328f69e0addadb8
Tuan Nguyen <ntuan.2502@gmail.com>
2022-02-18 11:23:25 +0700

    add new domain into next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require("./i18n.config");
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["uploadstatic-sea.hoyoverse.com"],
+    domains: ["uploadstatic-sea.hoyoverse.com", "webstatic-sea.hoyoverse.com"],
   },
   i18n,
 };
